f05474270f621a111a26e6001d187521914aadc4
Caitlin O'Halloran <cohalls@gmail.com>
2018-04-11 00:15:11 +0000

    Created Comments Model

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,9 @@ class Place < ApplicationRecord
   belongs_to :user
   validates :name, :address, :description, presence: true
   validates :name, length: { minimum: 3 }
+
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
 end
